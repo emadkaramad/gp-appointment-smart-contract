163598415ab62460c56703cfbd869a42d1c111b4
Emad Karamad <emad.karamad@gmail.com>
2023-03-18 23:44:19 +0000

    Update WalletManager to include deployer account

--- app/libs/WalletManager.ts
+++ app/libs/WalletManager.ts
@@ -1,19 +1,20 @@
 import { ethers, Wallet } from "ethers"
 
-export default class WalletManager {
-  private static readonly localStorageAccountName = "accounts"
-  private static readonly localStorageSelectedAccountName = "selected_account"
+const defaultMnemonic = "test test test test test test test test test test test junk"
+const localStorageAccountName = "accounts"
+const localStorageSelectedAccountName = "selected_account"
 
+export default class WalletManager {
   static setSelectedAccount(wallet: Wallet) {
     this.setSelectedMnemonic(wallet.mnemonic.phrase)
   }
 
   static setSelectedMnemonic(mnemonic: string) {
-    localStorage.setItem(this.localStorageSelectedAccountName, mnemonic)
+    localStorage.setItem(localStorageSelectedAccountName, mnemonic)
   }
 
   static getSelectedAccount(): Wallet | undefined {
-    const mnemonic = localStorage.getItem(this.localStorageSelectedAccountName)
+    const mnemonic = localStorage.getItem(localStorageSelectedAccountName)
     return mnemonic ? Wallet.fromMnemonic(mnemonic) : undefined
   }
 
@@ -32,14 +33,14 @@ export default class WalletManager {
     const mnemonics = this.getMnemonics()
     const updatedMnemonics = mnemonics.filter((x) => x !== mnemonic)
     localStorage.setItem(
-      this.localStorageAccountName,
+      localStorageAccountName,
       JSON.stringify(updatedMnemonics)
     )
   }
 
   static getMnemonics(): string[] {
-    let accounts: string[] = []
-    const storedAccounts = localStorage.getItem(this.localStorageAccountName)
+    let accounts: string[] = [defaultMnemonic]
+    const storedAccounts = localStorage.getItem(localStorageAccountName)
     if (storedAccounts) {
       accounts = JSON.parse(storedAccounts)
     }
@@ -50,6 +51,6 @@ export default class WalletManager {
   private static saveMnemonic(mnemonic: string) {
     const accounts = this.getMnemonics()
     accounts.push(mnemonic)
-    localStorage.setItem(this.localStorageAccountName, JSON.stringify(accounts))
+    localStorage.setItem(localStorageAccountName, JSON.stringify(accounts))
   }
 }
